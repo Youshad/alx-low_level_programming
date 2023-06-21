dcd0996bbc9c368309cc585d3d6cb36e5fffb366
Youshad <yousra3998@gmail.com>
2023-06-21 08:54:57 -0700

    11-print

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ */
+void print_number(int n)
+{
+	int num_digits = 0;
+
+	int tmp = n;
+
+	int div = 1;
+
+	int i;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (tmp != 0)
+	{
+		tmp /= 10;
+		num_digits++;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	for (i = 1; i < num_digits; i++)
+	{
+		div *= 10;
+	}
+	while (div != 0)
+	{
+		int digit = n / div;
+
+		_putchar('0' + digit);
+		n %= div;
+		div /= 10;
+	}
+}
+
+/**
+ * print_to_98 - prints all the intgers between an integer and 98
+ * @n: the starting integer
+ */
+void print_to_98(int n)
+{
+	int i;
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			print_number(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else {
+		for (i = n; i > 98; i--)
+		{
+			print_number(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	print_number(98);
+	_putchar('\n');
+}
